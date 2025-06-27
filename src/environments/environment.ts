96c88d5e6200496169a72681854eb4a3bd17278c
ranashahan <rana_shahan@hotmail.com>
2025-06-27 16:05:13 +0500

    Fixed certificates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/',
+  // apiUrl: '/api/',
   // apiUrl: 'http://192.168.0.143:5001/api/',
+  apiUrl: 'http://localhost:5001/api/',
   recaptchaSiteKey: '6Lc_SaYqAAAAAK8yHGjsFLe0RPDqFBlE2lPUAOXH',
 };
